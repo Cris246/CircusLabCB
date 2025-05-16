7adb641b0b71318df52516439e6418c8a6df919c
Cristina Bc <criisbc98@gmail.com>
2025-05-16 21:16:06 +0200

    html contact

--- frontend/src/app/components/contacto/contacto.component.ts
+++ frontend/src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faEnvelope, faLocationDot, faPhone} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+    imports: [
+        FaIconComponent
+    ],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
 
+  protected readonly faEnvelope = faEnvelope;
+  protected readonly faPhone = faPhone;
+  protected readonly faLocationDot = faLocationDot;
 }
