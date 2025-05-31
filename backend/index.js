11b7ee69490178907480c2b5293ae052c7e808a7
Karen Luik <criisbc98@gmail.com>
2025-05-31 13:16:21 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const path = require('path');
+const path = require('node:path');
 const app = express();
 const { mongoose } = require('./database');
 const { json } = require('express');
